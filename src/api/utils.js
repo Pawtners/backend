2552448f8a7b62674a533ac6dc34272a82bd4cd7
May Ng <mngmay@gmail.com>
2020-07-24 13:19:37 -0700

    validateAnimalId MW

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,5 +1,28 @@
+const Animals = require("./controllers/animalsController");
+
 const handleResponse = (res, code, statusMsg) => {
   res.status(code).json({ status: statusMsg });
 };
 
-module.exports = { handleResponse };
+// Middleware
+
+const validateAnimalId = async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const animal = await Animals.getAnimal(id);
+
+    if (animal.length) {
+      next();
+    } else {
+      return handleResponse(res, 404, {
+        message: `${id} is not a valid animal id.`,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return handleResponse(res, 500, { error: err });
+  }
+};
+
+module.exports = { handleResponse, validateAnimalId };
